8a115de8ed4fed274103acab0011680316cacd21
ovsbrian <ovsbrian@gmail.com>
2023-08-23 20:30:10 -0300

    add iconNav

--- src/assets/Componente/Secciones/SectionNav/Nav/ContainerIcoNText.jsx
+++ src/assets/Componente/Secciones/SectionNav/Nav/ContainerIcoNText.jsx
@@ -1,15 +1,15 @@
 import { IconText } from "./IconText"
-
+import {  CalendarDays, Home, LocateFixed, Map } from 'lucide-react';
 
 export const ContainerIconText = () => {
     return (
         <>
-            <div className="mt-10 flex flex-col justify-center items-center gap-4">
-              <IconText text={'uwu'}/>
-              <IconText text={'uwu'}/>
-              <IconText text={'uwu'}/>
-              <IconText text={'uwu'}/>
+            <div className="mt-10 p-1 flex flex-col justify-center items-center gap-8">
+              <IconText icon={<Home />} state={true} text={'Dashboard'}/>
+              <IconText icon={<Map />} text={'Map'}/>
+              <IconText icon={<LocateFixed />} text={'Saved Location'}/>
+              <IconText icon={<CalendarDays/>} text={'Calendar'}/>
             </div>
         </>
     )
-}
+} 
--- src/assets/Componente/Secciones/SectionNav/Nav/IconText.jsx
+++ src/assets/Componente/Secciones/SectionNav/Nav/IconText.jsx
@@ -1,9 +1,25 @@
-export const IconText = ({ icon, text }) => {
+import { useState } from "react";
+
+export const IconText = ({ icon, text, state }) => {
+  
+  const [focus, setFocus] = useState(state);
+  
+  const cambiarFocus = () => {
+    setFocus(!focus);
+  };
+
   return (
     <>
-      <div className="bg-slate-400 w-full">
-        <span>{icon}</span>
-        <span>{text}</span>
+      <div
+        className={` flex w-full items-center gap-1  ${
+          focus == true ? "opacity-100" : "opacity-50"
+        }`}
+        onClick={cambiarFocus}
+      >
+        <div className="w-8 hover:opacity-50 cursor-pointer ">{icon}</div>
+        <span className="text-base font-semibold hover:opacity-100 cursor-pointer w-full ">
+          {text}
+        </span>
       </div>
     </>
   );
